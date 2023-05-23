1ae976392ebf18938a783b0e6f556cb17132b2a1
gmx <gao_mx2021@163.com>
2023-05-23 21:12:08 +0800

    feat: vue版本翻转时钟

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import FlipCalendar from './components/FlipCalendar.vue';
+</script>
+
+<template>
+<FlipCalendar/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: gmx
+ * @Date: 2023-05-21 15:22:43
+ * @LastEditors: gmx
+ * @LastEditTime: 2023-05-21 15:28:38
+ * @Description: 
+ * @Version: 1.0
+ */
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
