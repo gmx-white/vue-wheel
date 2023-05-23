a9397ef1f8d8fc53093c33df867476c61660902c
gmx <gao_mx2021@163.com>
2023-05-23 21:24:48 +0800

    delete: 其他分支代码

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
+<!--
+ * @Author: gmx
+ * @Date: 2023-05-21 15:22:43
+ * @LastEditors: gmx
+ * @LastEditTime: 2023-05-23 21:24:14
+ * @Description: 
+ * @Version: 1.0
+-->
 <script setup>
-import FlipCalendar from './components/FlipCalendar.vue';
 </script>
 
 <template>
-<FlipCalendar/>
 </template>
 
 <style scoped>
